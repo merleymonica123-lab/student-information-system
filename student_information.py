3b1af72e3f1065751b6a20c758bc764ddbd41e42
Monica Mamah <merleymonica123>
2026-08-24 22:08:49 +0000

    Add generated username and email

--- student_information.py
+++ student_information.py
@@ -6,3 +6,8 @@ programme = input("Enter your programme: ")
 level = input("Enter your level: ")
 age = input("Enter your age: ")
 favourite_language = input("Enter your favourite programming language: ")
+name_part = full_name[:3].lower()
+username = name_part + student_id
+
+domain = "@st.ug.edu.gh"
+email = username + domain
